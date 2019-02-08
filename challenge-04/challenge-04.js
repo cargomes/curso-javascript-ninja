8eec930202cf0e0112b23858b14c5f11a3c3dc9e
Carlos André <carlosandre.web@gmail.com>
2019-02-08 21:59:53 -0200

    CHALLENGE-04

---
+++ challenge-04/challenge-04.js
@@ -0,0 +1,257 @@
+# Desafio da semana #4
+
+
+/*
+Declare uma variável chamada `isTruthy`, e atribua a ela uma função que recebe
+um único parâmetro como argumento. Essa função deve retornar `true` se o
+equivalente booleano para o valor passado no argumento for `true`, ou `false`
+para o contrário.
+*/
+
+var isTruthy = function(paramentro) {
+
+	if (paramentro) {
+
+		return true;
+	}
+
+	return false;
+
+};
+
+var isTruthy = function(paramentro) {
+
+return paramentro ? true : false;
+
+};
+
+var isTruthy = function(paramentro) {
+
+return !!paramentro;
+
+};
+
+
+// Invoque a função criada acima, passando todos os tipos de valores `falsy`.
+
+isTruthy (false);
+isTruthy (null);
+isTruthy (undefined);
+isTruthy ('');
+isTruthy (0);
+isTruthy(-0);
+isTruthy (NaN);
+
+
+/*
+Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
+*/
+?
+isTruthy (true);
+isTruthy ('carlos');
+isTruthy (1);
+isTruthy (5 + 5);
+isTruthy (2 === 2);
+isTruthy({});
+isTruthy (function() {});
+isTruthy ([]);
+
+/*
+Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
+seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
+- `marca` - String
+- `modelo` - String
+- `placa` - String
+- `ano` - Number
+- `cor` - String
+- `quantasPortas` - Number
+- `assentos` - Number - cinco por padrão
+- `quantidadePessoas` - Number - zero por padrão
+*/
+
+/*----------------------
+criando objeto carro
+------------------------*/
+
+var carro = {      /// variavel  'var carro'
+
+ marca:"fiat",
+ modelo:'Bravo',
+ placa:'jgf-7648',  ///propriedades de objeto separa por virgula,
+ ano: 2013,         /// a última não precisa         
+ cor: 'Azul',
+ portas:4,
+ assentos:5,
+ qtdePessoa:0
+
+};
+
+/*
+Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor
+passado por parâmetro.
+*/
+
+/// isso é um método (representa uma ação), 
+//usar sempre verbo mudarCor
+carro.mudarCor = function (cor) {
+
+	carro.cor = cor;
+
+};
+
+/*
+Crie um método chamado `obterCor`, que retorne a cor do carro.
+*/
+
+// método sem parametro, apenas trás a cor
+ carro.obterCor = function () {
+
+return carro.cor;
+
+ };
+
+/*
+Crie um método chamado `obterModelo` que retorne o modelo do carro.
+*/
+
+carro.obterModelo = function (){
+
+	return carro.modelo;
+};
+
+
+/*
+Crie um método chamado `obterMarca` que retorne a marca do carro.
+*/
+
+carro.obterMarca = function () {
+
+	return carro.marca;
+};
+
+
+
+/*
+Crie um método chamado `obterMarcaModelo`, que retorne:
+"Esse carro é um [MARCA] [MODELO]"
+Para retornar os valores de marca e modelo, utilize os métodos criados.
+*/
+
+carro.obterMarcaModelo = function () {
+
+ return 'Esse carro é um ' + carro.obterMarca() + ' ' + carro.obterModelo();
+
+};
+
+/*
+Crie um método que irá adicionar pessoas no carro. Esse método terá as
+seguintes características:
+- Ele deverá receber por parâmetro o número de pessoas entrarão no carro. Esse
+número não precisa encher o carro, você poderá acrescentar as pessoas aos
+poucos.
+- O método deve retornar a frase: "Já temos [X] pessoas no carro!"
+- Se o carro já estiver cheio, com todos os assentos já preenchidos, o método
+deve retornar a frase: "O carro já está lotado!"
+- Se ainda houverem lugares no carro, mas a quantidade de pessoas passadas por
+parâmetro for ultrapassar o limite de assentos do carro, então você deve
+mostrar quantos assentos ainda podem ser ocupados, com a frase:
+"Só cabem mais [QUANTIDADE_DE_PESSOAS_QUE_CABEM] pessoas!"
+- Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
+citado acima, no lugar de "pessoas".
+*/
+
+carro.adicionarPessoas = function (numberpeople){
+
+var totalPessoas = carro.qtdePessoa + numberpeople;
+var vagas = carro.assentos - carro.qtdePessoa;
+var pluralSingular = vagas === 1 ? ' pessoa' : ' pessoas';
+
+if (carro.qtdePessoa === carro.assentos) { 
+
+return "O carro já está lotado!";
+
+}
+
+if ( totalPessoas > carro.assentos){
+
+	return 'Só cabem mais ' + vagas + pluralSingular + '!';
+}
+
+carro.qtdePessoa += numberpeople;
+return 'Já temos ' + totalPessoas + ' pessoas no carro!';
+
+};
+
+/*
+Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
+utilize sempre o formato de invocação do método (ou chamada da propriedade),
+adicionando comentários _inline_ ao lado com o valor retornado, se o método
+retornar algum valor.
+
+Qual a cor atual do carro?
+*/
+
+carro.cor //"Azul"
+
+// Mude a cor do carro para vermelho.
+
+carro.mudarCor = function (cor) {
+
+	carro.cor = cor;
+
+}; 
+  carro.mudarCor('rosa'); // 
+
+// E agora, qual a cor do carro?
+?
+
+// Mude a cor do carro para verde musgo.
+
+
+// E agora, qual a cor do carro?
+?
+
+// Qual a marca e modelo do carro?
+?
+
+// Adicione 2 pessoas no carro.
+?
+
+// Adicione mais 4 pessoas no carro.
+?
+
+// Faça o carro encher.
+?
+
+// Tire 4 pessoas do carro.
+?
+
+// Adicione 10 pessoas no carro.
+?
+
+// Quantas pessoas temos no carro?
+?
+
+
+
+//// treinos
+
+function dividir (num1, num2) {
+
+	return num1 * num2;
+}
+
+alert ('O resultado da divisão é: ' + dividir(15,2));
+
+///////////////////////////////////////
+
+
+function dividir (num1, num2) {
+
+var resultado =  num1 * num2;
+return 'O resultado da divisão é: ' + resultado;
+
+}
+
+dividir (15,10);
+"O resultado da divisão é: 150"
